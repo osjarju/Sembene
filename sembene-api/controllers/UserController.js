2c3f528d4272ea96b78db94c7da9e3b6915271c5
osjarju <115750838+osjarju@users.noreply.github.com>
2023-04-11 20:36:03 -0700

    Server Code Partially Broken

--- sembene-api/controllers/UserController.js
+++ sembene-api/controllers/UserController.js
@@ -1,1 +1,9 @@
-const User = require('../models/UserModel');
+// const User = require('../models/UserModel');
+// module.exports.addToLikedMovies = async (req, res) => {
+//     try {
+//         const { email, data } = req.body;
+//         const user = await User.findOne({ email });
+//     } catch (errors) {
+//         return res.json({ 'error adding movie'});
+//     }
+// };
--- sembene-api/server.js
+++ sembene-api/server.js
@@ -1,18 +1,23 @@
+//IMPORT DEPENDENCIES
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const UserRoutes = require('./routes/UserRoute');
 
 const app = express();
+
+// app.use('/api/user/UserRoute');
 
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_URL)
+//CONNECT MONGODB
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+    //LOG EXECUTION & HANDLE ERRORS
     .then(() => {
         console.log('Database Connected');
         app.listen(5000, () => console.log('Server Listening on port 5000'))
